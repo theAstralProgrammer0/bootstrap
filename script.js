8cde4475f5b9b55d3504db5dd3c5dcb8e4008d61
Temitayo Daisi-Oso <tdaisioso@gmail.com>
2022-07-08 12:31:55 +0100

    Added bootstrap jquery code to control carousel cycle on mouse entry and mouse exit of the carousel container

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#myCarousel").carousel({interval:2000});
+    $("#myCarousel").mouseenter(function(){
+        $("#myCarousel").carousel("pause");
+        $("#myCarousel").mouseleave(function(){
+            $("#myCarousel").carousel("cycle");
+        });
+    });
+})
